8da4a4ebc24b9f2fea45521c434c9d1e9baf6439
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-11-01 14:32:35 +0000

    Create/update profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,27 +1,105 @@
-const express = require('express');
-const auth = require('../../middleware/auth');
+const express = require("express");
+const auth = require("../../middleware/auth");
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
 
-const Profile = require('../../models/Profile');
-const User = require('../../models/User');
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
 
 // @route   GET api/profile/me
 // @desc    Get current users profile
 // @access  PRIVATE
 
-router.get('/me', auth, async (req, res) => {
-    try {
-        const profile = await Profile.findOne({ user: req.user.id }).populate('User', ['name', 'avatar']);
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      "User",
+      ["name", "avatar"]
+    );
 
-        if (!profile) {
-            return res.status(400).json({ msg: 'No profile found for this user' });
-        }
-
-        res.json(profile);
-    } catch (e) {
-        console.error(e.message);
-        res.status(500).send('Server Error');
+    // If no profile exists
+    if (!profile) {
+      return res.status(400).json({ msg: "No profile found for this user" });
     }
+
+    res.json(profile);
+  } catch (e) {
+    console.error(e.message);
+    res.status(500).send("Server Error");
+  }
 });
+
+// @route   POST api/profile
+// @desc    Create or update user profile
+// @access  PRIVATE
+
+router.post(
+  "/",
+  auth,
+  body("skills", "Skills is required").notEmpty(),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    // Destructure request
+    const {
+      website,
+      skills,
+      youtube,
+      facebook,
+      linkedin,
+      instagram,
+      twitter,
+      bio,
+      githubusername,
+    } = req.body;
+
+    // Build profile object
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (website) profileFields.website = website;
+    if (bio) profileFields.bio = bio;
+    if (githubusername) profileFields.githubusername = githubusername;
+    if (skills) {
+      profileFields.skills = skills.split(",").map(skill => skill.trim());
+    }
+
+    // Build social object
+    profileFields.social = {};
+    if (youtube) profileFields.social.youtube = youtube;
+    if (twitter) profileFields.social.twitter = twitter;
+    if (instagram) profileFields.social.instagram = instagram;
+    if (linkedin) profileFields.social.linkedin = linkedin;
+    if (facebook) profileFields.social.facebook = facebook;
+
+    try {
+      let profile = await Profile.findOne({ user: req.user.id });
+
+      // If profile found
+      if (profile) {
+        // Update
+        profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        );
+
+        return res.json(profile);
+      }
+
+      // Create profile
+      profile = new Profile(profileFields);
+
+      await profile.save();
+
+      res.json(profile);
+    } catch (e) {
+      console.error(e.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,77 +1,90 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const gravatar = require('gravatar');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
-const config = require('config');
-const { body, validationResult } = require('express-validator');
+const gravatar = require("gravatar");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const config = require("config");
+const { body, validationResult } = require("express-validator");
 
-const User = require('../../models/User');
+const User = require("../../models/User");
 
-// @route   POST api/users 
+// @route   POST api/users
 // @desc    Register user
 // @access  PUBLIC
 
-router.post('/', [
+router.post(
+  "/",
+  [
     // Validation using express validator
-    body('name', 'Name is required').not().isEmpty(),
-    body('email', 'Please include a valid email').isEmail(),
-    body('password', 'Please enter a password with 6 or more characters').isLength({min:6})
-], async (req, res) => {
+    body("name", "Name is required").notEmpty(),
+    body("email", "Please include a valid email").isEmail(),
+    body(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({ errors: errors.array() });
     }
 
     const { name, email, password } = req.body;
 
     try {
-        // See if user exists already
-        let user = await User.findOne({ email });
+      // See if user exists already
+      let user = await User.findOne({ email });
 
-        if (user) {
-            return res.status(400).json({ errors: [ { msg: 'User already registered' }] });
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already registered" }] });
+      }
+
+      // Get users gravatar
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm",
+      });
+
+      // Create new user instance
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
+      // Encrypt password
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      // Save new user to db
+      await user.save();
+
+      // Return jsonwebtoken
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
         }
-
-        // Get users gravatar
-        const avatar = gravatar.url(email, {
-            s: '200',
-            r: 'pg',
-            d: 'mm'
-        });
-
-        // Create new user instance
-        user = new User({
-            name,
-            email,
-            avatar,
-            password
-        });
-
-        // Encrypt password
-        const salt = await bcrypt.genSalt(10);
-
-        user.password = await bcrypt.hash(password, salt);
-
-        // Save new user to db
-        await user.save();
-
-        // Return jsonwebtoken
-        const payload = {
-            user: {
-                id: user.id
-            }
-        }
-
-        jwt.sign(payload, config.get('jwtSecret'), { expiresIn: 360000 }, (err, token) => {
-            if (err) throw err;
-            res.json({ token })
-        });
-
-    } catch(e) {
-        console.error(e.message);
-        res.status(500).send("Server Error");
+      );
+    } catch (e) {
+      console.error(e.message);
+      res.status(500).send("Server Error");
     }
-});
+  }
+);
 
 module.exports = router;

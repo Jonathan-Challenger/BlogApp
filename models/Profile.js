bf9704593a1779f93f5aa6c3ed61495562c1103d
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-11-01 12:11:07 +0000

    Create Profile schema

---
+++ models/Profile.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const ProfileSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    website: {
+        type: String
+    },
+    bio: {
+        type: String
+    },
+    skills: {
+        type: [String],
+        required: true
+    },
+    githubusername: {
+        type: String
+    },
+    social: {
+        youtube: {
+            type: String
+        },
+        twitter: {
+            type: String
+        },
+        instagram: {
+            type: String
+        },
+        facebook: {
+            type: String
+        },
+        linkedin: {
+            type: String
+        },
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = Profile = mongoose.model('Profile', ProfileSchema);

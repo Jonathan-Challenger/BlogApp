669e508bbfb3facfa5562fdc6eb07000eb917cfb
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-11-01 12:25:42 +0000

    add logic to profile route to get the logged in users profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,13 +1,27 @@
 const express = require('express');
-
+const auth = require('../../middleware/auth');
 const router = express.Router();
 
-// @route   GET api/profile 
-// @desc    Test route
-// @access  PUBLIC
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
-router.get('/', (req, res) => {
-    res.send("Profile route");
+// @route   GET api/profile/me
+// @desc    Get current users profile
+// @access  PRIVATE
+
+router.get('/me', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id }).populate('User', ['name', 'avatar']);
+
+        if (!profile) {
+            return res.status(400).json({ msg: 'No profile found for this user' });
+        }
+
+        res.json(profile);
+    } catch (e) {
+        console.error(e.message);
+        res.status(500).send('Server Error');
+    }
 });
 
 module.exports = router;

293813aa7720729c49c171bb2b14265b02112c69
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-11-05 11:23:55 +0000

    Create login page

--- client/src/components/auth/Login.jsx
+++ client/src/components/auth/Login.jsx
@@ -1,8 +1,57 @@
+import { Link } from "react-router-dom";
+import { useState } from "react";
+
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = formData;
+
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async e => {
+    e.preventDefault();
+
+    console.log("SUCCESS");
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <>
+      <h1 className='large text-primary'>Login</h1>
+      <p className='lead'>
+        <i className='fas fa-sign-in-alt'></i> Log In Here
+      </p>
+      <form className='form' onSubmit={e => onSubmit(e)}>
+        <div className='form-group'>
+          <input
+            type='email'
+            placeholder='Email Address'
+            name='email'
+            value={email}
+            onChange={e => onChange(e)}
+            required
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            value={password}
+            onChange={e => onChange(e)}
+            minLength='6'
+            required
+          />
+        </div>
+        <input type='submit' className='btn btn-primary' value='Login' />
+      </form>
+      <p className='my-1'>
+        Need an account? <Link to='/register'>Sign Up Here</Link>
+      </p>
+    </>
   );
 };
 

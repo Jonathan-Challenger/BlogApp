c283621dfd0f22e6819dbe6eb3d5819c202f0fe1
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-11-05 14:56:21 +0000

    change token expiresIn to 1 week

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -70,7 +70,7 @@ router.post(
       jwt.sign(
         payload,
         config.get("jwtSecret"),
-        { expiresIn: 360000 },
+        { expiresIn: 604800 },
         (err, token) => {
           if (err) throw err;
           res.json({ token });

9cd814b947508a65160260c0ca7a6751b13fd7be
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-11-08 14:03:43 +0000

    load user and set auth token

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,22 @@ import Landing from "./components/layout/landing/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Alert from "./components/layout/Alert";
+import { loadUser } from "./actions/auth";
+import setAuthToken from "./utils/setAuthToken";
+import { useEffect } from "react";
 // Redux
 import { Provider } from "react-redux";
 import store from "./store";
 
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
 const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
   return (
     <Provider store={store}>
       <Router>

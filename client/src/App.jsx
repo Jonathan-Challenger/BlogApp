c492548ac85341d25915c6b95c3393d57e826b0c
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-11-11 14:08:26 +0000

    create profile component to store indivdual profile pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,6 +13,7 @@ import PrivateRoute from "./components/routing/PrivateRoute";
 import CreateProfile from "./components/profile-form/CreateProfile";
 import EditProfile from "./components/profile-form/EditProfile";
 import Profiles from "./components/profiles/Profiles";
+import Profile from "./components/profile/Profile";
 // Redux
 import { Provider } from "react-redux";
 import store from "./store";
@@ -38,6 +39,7 @@ const App = () => {
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
               <Route exact path='/profiles' component={Profiles} />
+              <Route exact path='/profile/:id' component={Profile} />
               <PrivateRoute exact path='/dashboard' component={Dashboard} />
               <PrivateRoute
                 exact

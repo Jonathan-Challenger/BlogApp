c3c88d6a36fff206aa8914cb15dbd7d085904a15
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-11-16 14:12:48 +0000

    Create PostItem component

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -3,13 +3,29 @@ import { useEffect } from "react";
 import { connect } from "react-redux";
 import { getPosts } from "../../actions/post";
 import Spinner from "../layout/Spinner";
+import PostItem from "./PostItem";
 
 const Posts = ({ post: { posts, loading }, getPosts }) => {
   useEffect(() => {
     getPosts();
   }, [getPosts]);
 
-  return <div></div>;
+  return loading ? (
+    <Spinner />
+  ) : (
+    <section className='container'>
+      <h1 className='large text-primary'>Posts</h1>
+      <p className='lead'>
+        <i className='fas fa-user'></i> Welcome to the community
+      </p>
+      {/* Post Form */}
+      <div className='posts'>
+        {posts.map(post => (
+          <PostItem key={post._id} post={post} />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 Posts.propTypes = {

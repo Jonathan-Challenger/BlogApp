c9bb857dcf1b05744b1cb18c00c268144b91bdf8
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-11-17 11:22:43 +0000

    remove post functionality

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { setAlert } from "./alert";
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from "./types";
+import { DELETE_POST, GET_POSTS, POST_ERROR, UPDATE_LIKES } from "./types";
 
 // Get posts
 export const getPosts = () => async dispatch => {
@@ -51,6 +51,28 @@ export const removeLike = id => async dispatch => {
       type: UPDATE_LIKES,
       payload: { id, likes: res.data },
     });
+  } catch (error) {
+    dispatch({
+      type: POST_ERROR,
+      payload: {
+        msg: error.response.statusText,
+        status: error.response.status,
+      },
+    });
+  }
+};
+
+// Delete post
+export const deletePost = id => async dispatch => {
+  try {
+    const res = await axios.delete(`/api/posts/${id}`);
+
+    dispatch({
+      type: DELETE_POST,
+      payload: id,
+    });
+
+    dispatch(setAlert("Post removed", "success"));
   } catch (error) {
     dispatch({
       type: POST_ERROR,

7c9a6dc67289a12c2b2b13d1b6b31b6c50f22f8f
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2021-11-19 11:03:44 +0000

    Add Not found page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,6 +16,7 @@ import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
 import Posts from "./components/posts/Posts";
 import Post from "./components/post/Post";
+import NotFound from "./components/layout/NotFound";
 // Redux
 import { Provider } from "react-redux";
 import store from "./store";
@@ -50,6 +51,7 @@ const App = () => {
             <PrivateRoute exact path='/edit-profile' component={EditProfile} />
             <PrivateRoute exact path='/posts' component={Posts} />
             <PrivateRoute exact path='/posts/:id' component={Post} />
+            <Route component={NotFound} />
           </Switch>
         </>
       </Router>
